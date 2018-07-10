471e45114c08450823edb40717d6f70c96299e9b
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-10 23:56:02 +0200

    1st solution - 1st commit

---
+++ string_reversal.js
@@ -0,0 +1,14 @@
+// REVERSE A STRING
+// ex. reverseString('hello') == 'olleh'
+
+//1
+function reverseString(str) {
+    const strArray = str.split('');
+    strArray.reverse();
+
+    return strArray.join('');
+}
+
+
+const output = reverseString('pink_duck');
+console.log(output);
