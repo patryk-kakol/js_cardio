f159ce5dc7c811df5ce168d8978dad9cd7f45edf
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-11 00:58:36 +0200

    2nd solution

--- string_reversal.js
+++ string_reversal.js
@@ -1,14 +1,21 @@
 // REVERSE A STRING
 // ex. reverseString('hello') == 'olleh'
 
-// 1
-function reverseString(str) {
-  const strArray = str.split('');
-  strArray.reverse();
-
-  return strArray.join('');
+// 1 - using reverse() is obvious
+function reverseString1(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+// 2 - for loop reversal (decremental)
+function reverseString2(str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
-
-const output = reverseString('pink_duck');
+const output = reverseString2('pink_duck');
 console.log(output);
