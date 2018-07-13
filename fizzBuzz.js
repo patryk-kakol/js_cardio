4f4aaadeaa442c84b330da609111d772472df8ea
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 00:21:36 +0200

    1st solution

---
+++ fizzBuzz.js
@@ -0,0 +1,21 @@
+/* everybody knows this one!
+  basically prints all numbers from 1 to 100,
+  but for multiples of 3, insted of number, it should print "Fizz"
+  also for multiples of 5 should print "Buzz"
+  and for multiples of both print "FizzBuzz" */
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i += 1) {
+    if (i % (3 * 5) === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
