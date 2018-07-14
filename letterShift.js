5d0c7ad696396e71c9b20a7bd2f8d3fcdfb67b36
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 21:50:18 +0200

    test

---
+++ letterShift.js
@@ -0,0 +1,8 @@
+/* Shift letter to next in ASCII table, in case of Z go back to A
+  ex. letterShift('zebra crossing') === 'afcsb dspttjoh' */
+
+function letterShift(str) {
+
+}
+
+console.log(letterShift('zebra crossing'));
