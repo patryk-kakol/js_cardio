295369358feac1b6dbe6824ba28414b6e0bf31cb
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 18:03:10 +0200

    4th solution

---
+++ flattenArray.js
@@ -0,0 +1,36 @@
+/* Takes an array of array and flatten to single array
+  ex. flattenArr([1, [2, 3], [4, 5, 6], 7]) === [1, 2, 3, 4, 5, 6, 7] */
+
+// 1 - reduce()
+function flattenArr1(arr) {
+  return arr.reduce((a, b) => a.concat(b), []);
+}
+
+// 2 - apply(), eslint does not like it, me neither, but it works
+function flattenArr2(arr) {
+  return [].concat.apply([], arr);
+}
+
+// 3 - spread operator, probably best solution
+function flattenArr3(arr) {
+  return [].concat(...arr);
+}
+
+/* 4 - join, split and map to numbers,
+  good if nesting array things go to the next level,
+  where previous solution fail */
+function flattenArr4(arr) {
+  return arr.join().split(',').map(a => +a);
+}
+
+console.log(flattenArr1([1, [2, 3], [4, [5], 6], 7]));
+console.log(flattenArr2([1, [2, 3], [4, [5], 6], 7]));
+console.log(flattenArr3([1, [2, 3], [4, [5], 6], 7]));
+console.log(flattenArr4([1, [2, 3], [4, [5], 6], 7]));
+console.log(flattenArr1([1, [2, 3], [4, 5, 6], 7]));
+console.log(flattenArr2([1, [2, 3], [4, 5, 6], 7]));
+console.log(flattenArr3([1, [2, 3], [4, 5, 6], 7]));
+console.log(flattenArr4([1, [2, 3], [4, 5, 6], 7]));
+
+
+console.log(typeof ([1, 3, 4, [5, 6]]));
