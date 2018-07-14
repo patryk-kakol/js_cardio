b48e9c2d72375825972c96854066a01da3f1c25c
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 22:04:46 +0200

    1st solution

--- letterShift.js
+++ letterShift.js
@@ -1,8 +1,16 @@
 /* Shift letter to next in ASCII table, in case of Z go back to A
   ex. letterShift('zebra crossing') === 'afcsb dspttjoh' */
 
+/* 1 - linter didn't liked 'else' here,
+  arrow + ternary is considered confusing, so this is best i got,
+  works, and 0 linter errors */
 function letterShift(str) {
-
+  return str
+    .toLowerCase()
+    .replace(/[a-z]/g, (char) => {
+      if (char !== 'z') return String.fromCharCode(char.charCodeAt() + 1);
+      return 'a';
+    });
 }
 
 console.log(letterShift('zebra crossing'));
