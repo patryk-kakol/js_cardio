effe4317e1e24ce9351a56c7c635f2b75c4d7770
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 14:16:06 +0200

    2nd solution

--- longestWord.js
+++ longestWord.js
@@ -17,4 +17,18 @@ function longestWord1(str) {
   return result;
 }
 
+/* 2 - with use of sort and filter insted of array iteration,
+  additionally if theres more then one word of max length
+  it will return an array containing all longest words */
+function longestWord2(str) {
+  const sorted = str.match(/[A-Za-z0-9]+/g)
+    .sort((a, b) => b.length - a.length);
+
+  const longestArr = sorted
+    .filter(word => word.length === sorted[0].length);
+
+  return longestArr.length === 1 ? longestArr[0] : longestArr;
+}
+
 console.log(longestWord1('Hello there, my name is Patryk'));
+console.log(longestWord2('Helloo there, my name is Patryk'));
