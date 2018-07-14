bacbdb389b00b4bcc4891f7b0a0991ef37aea55c
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-14 16:28:23 +0200

    2nd solution

--- chunkArray.js
+++ chunkArray.js
@@ -2,7 +2,7 @@
   with length specified as parameter in function call
   ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2], [3, 4], [5, 6], [7]] */
 
-// 1
+// 1 - keeping in mind that array iterations are light side
 function chunkArray1(arr, len) {
   const chunked = [];
 
@@ -17,4 +17,17 @@ function chunkArray1(arr, len) {
   return chunked;
 }
 
+// 2 - slice the complete chunks
+function chunkArray2(arr, len) {
+  const chunked = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    chunked.push(arr.slice(i, i + len));
+    i += len;
+  }
+  return chunked;
+}
+
 console.log(chunkArray1([1, 2, 3, 4, 5, 6, 7], 2));
+console.log(chunkArray2([1, 2, 3, 4, 5, 6, 7], 2));
