f5f070c8be4a3684383a107e3aa86340b618f0ab
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-16 22:53:56 +0200

    array operation

--- io/fileReading.js
+++ io/fileReading.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 // console.log(content);
 
 // asynchronus way
-
 let content = 'initial value';
 fs.readFile('sample.txt', 'utf8', (error, result) => {
   content = result;
   console.log(content);
+  content
+    .split('\n')
+    .filter(line => line.includes('animal'))
+    .forEach((line, index) => console.log('animal ' + (index + 1) + ' : ' + line));
 });
 console.log(content);
