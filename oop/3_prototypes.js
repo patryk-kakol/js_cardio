09d9172a957c324711529d515af25e1ed727798c
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-17 23:50:41 +0200

    more examples

--- oop/3_prototypes.js
+++ oop/3_prototypes.js
@@ -8,6 +8,16 @@ function Book(title, author, year) {
 Book.prototype.getSummary = function () {
   return `${this.title} was written by ${this.author} in ${this.year}`;
 };
+// getAge
+Book.prototype.getAge = function () {
+  const years = new Date().getFullYear() - this.year;
+  return `${this.title} is ${years} years old`;
+};
+// revise / change year
+Book.prototype.revise = function (newYear) {
+  this.year = newYear;
+  this.revised = true;
+}
 
 // instatiate an object
 const book1 = new Book('Book One', 'John Doe', '2013');
@@ -15,3 +25,8 @@ const book2 = new Book('Book Two', 'Jane Doe', '2016');
 console.log(book1);
 console.log(book2);
 console.log(book1.getSummary());
+console.log(book1.getAge());
+
+book1.revise('2017');
+console.log(book1.getAge());
+console.log(book1);
