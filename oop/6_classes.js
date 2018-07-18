745bb7f917e3fab8724a35db2c720e340ae11633
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-18 23:05:42 +0200

    added methods

--- oop/6_classes.js
+++ oop/6_classes.js
@@ -4,10 +4,27 @@ class Book {
     this.author = author;
     this.year = year;
   }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+
+  getAge() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+  }
+
+  revise(newYear) {
+    this.year = newYear;
+    this.revised = true;
+  }
 }
+
 
 // instantiate object
 const book1 = new Book('Book One', 'John Doe', '2013');
 
-
 console.log(book1);
+console.log(book1.getSummary());
+book1.revise('2015');
+console.log(book1.getAge());
