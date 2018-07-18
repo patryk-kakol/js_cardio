6df39cc4cddf957b1cb3a074eccc7026bc4d392f
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-18 23:00:37 +0200

    initial

---
+++ oop/6_classes.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+}
+
+// instantiate object
+const book1 = new Book('Book One', 'John Doe', '2013');
+
+
+console.log(book1);
