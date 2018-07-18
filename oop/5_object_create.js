a89200851bce51d4e299e4bcbee9dac7d774e982
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-18 22:56:22 +0200

    initial

---
+++ oop/5_object_create.js
@@ -0,0 +1,26 @@
+// Object of protos
+const bookProtos = {
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+  getAge() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+  },
+};
+
+// create object
+const book1 = Object.create(bookProtos);
+book1.title = 'Book One';
+book1.author = 'John Doe';
+book1.year = '2013';
+
+// other way
+const book2 = Object.create(bookProtos, {
+  title: {value: 'Book Two'},
+  author: {value: 'Jane Doe'},
+  year: {value: '2013'}
+});
+
+console.log(book1.getSummary());
+console.log(book2.getSummary());
