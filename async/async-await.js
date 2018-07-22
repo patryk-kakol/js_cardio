5c740d54c41eee11c5582402e1a56513733c1140
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-23 00:14:44 +0200

    fetch example

--- async/async-await.js
+++ async/async-await.js
@@ -37,3 +37,13 @@ async function init() {
 }
 
 init();
+
+// Async/Await + Fetch
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  console.log(data);
+}
+
+fetchUsers();
