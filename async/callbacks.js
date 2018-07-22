0b68b8085239ae6e23c74aeb570d12f27886f71f
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-22 23:25:20 +0200

    done

--- async/callbacks.js
+++ async/callbacks.js
@@ -11,12 +11,13 @@ function getPosts() {
   }, 1000);
 }
 
-function createPost(post) {
+function createPost(post, callback) {
   setTimeout(() => {
     posts.push(post);
+    callback();
   }, 2000);
 }
 
 getPosts();
 
-createPost({ title: 'Post threee', body: 'This is post three'});
+createPost({ title: 'Post three', body: 'This is post three' }, getPosts);
