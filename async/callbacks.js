b2494a6e4d3e6c10b04906ac6367db1edbc5e4cb
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-22 23:21:51 +0200

    no callback

---
+++ async/callbacks.js
@@ -0,0 +1,22 @@
+const posts = [
+  { title: 'Post one', body: 'This is post one' },
+  { title: 'Post two', body: 'This is post two' },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach(post => output += `<li>${post.title}</li>`);
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
+getPosts();
+
+createPost({ title: 'Post threee', body: 'This is post three'});
