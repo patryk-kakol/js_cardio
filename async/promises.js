b2d04428cc3349ea60bd3485cd16183d753b6dda
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-22 23:41:19 +0200

    initial

---
+++ async/promises.js
@@ -0,0 +1,33 @@
+const posts = [
+  { title: 'Post one', body: 'This is post one' },
+  { title: 'Post two', body: 'This is post two' },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach(post => output += `<li>${post.title}</li>`);
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject('Error: Something went wrong! Panic!');
+      }
+    }, 2000);
+  });
+}
+
+getPosts();
+
+createPost({ title: 'Post three', body: 'This is post three' })
+  .then(getPosts);
