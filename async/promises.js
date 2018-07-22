56808e3bcbc5a0d3203887234cc44e9622a10dee
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-22 23:59:11 +0200

    Promise.all()

--- async/promises.js
+++ async/promises.js
@@ -33,3 +33,14 @@ getPosts();
 createPost({ title: 'Post three', body: 'This is post three' })
   .then(getPosts)
   .catch(err => console.log(err));
+
+
+// Promise.all
+const promise1 = Promise.resolve('Yo!');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json());
+
+Promise.all([promise1, promise2, promise3, promise4])
+  .then(values => console.log(values));
